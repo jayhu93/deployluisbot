7b1832199025c5542fd5ee7d8bf4ceb6d9457076
Jeremy Chien <jchien15@gmail.com>
2017-04-01 22:25:11 -0700

    power rangers

--- app.js
+++ app.js
@@ -28,139 +28,58 @@ var recognizer = new builder.LuisRecognizer(process.env.LUIS_MODEL_URL);
 bot.recognizer(recognizer);
 
 var questions = {
-    'How do you handle setbacks':"Suggestions for setbacks",
-    'What are your greatest weakness':'Suggestions for greatest weakness',
-    'What is your career goal': 'suggestions for career goal'
+    'How do you handle setbacks?': 'Suggestions for setbacks',
+    'What are your greatest weaknesses?': 'Suggestions for greatest weaknesses',
+    'What are your career goals?': 'Suggestions for career goals'
 };
-var questionsArray = Object.keys(questions);
 
+
+
+
+var questionsArray = Object.keys(questions);
+questionsArray.sort( function(a, b) { return 0.5 - Math.random() });
 
 var counter = 0;
 
 bot.dialog('StartInterview', function (session, args, next) {
-    // questions.sort(function(a,b) {return 0.5-Math.random()});
-            session.send(questionsArray[0]);
-            // session.endDialog();
+    session.send('Okay! Let me ask you a few questions.');
+    if (counter < questionsArray.length)
+        session.send(questionsArray[counter]);
+    else {
+        session.send('Congratulations. You have completed the interview. Thank you for your time.');
+        counter = 0;
+    }
     }).triggerAction({
     matches: 'StartInterview'
 });
 
+bot.dialog('Greeting', function (session, args, next) {
+    session.send('Hello, I am Robodachi! Would you like me to help you with your interview?')
+}).triggerAction({
+    matches: 'Greeting'
+});
 
 
 bot.dialog('Good', function (session, args, next) {
-    session.send('This is a good answer');
-    session.send(questions[questionsArray[0]]);
-    session.send(questionsArray[1]);
+    session.send('This is a great answer! However, here are some other suggestions! ' + questions[questionsArray[counter]]);
+    session.send(questionsArray[++counter]);
+    if (counter >= questionsArray.length) {
+        session.send('Congratulations. You have completed the interview. Thank you for your time.');
+        counter = 0;
+    }
     // session.beginDialog('StartInterview');
 }).triggerAction({
     matches: 'Good'
 });
 
 bot.dialog('Bad', function(session, args, next) {
-    session.send('This is a bad answer');
-    session.send(questions[questionsArray[0]]);
-    session.send(questionsArray[1]);
+    session.send('Okay, this might not be the best answer, here are some suggestions to improve your answer. ' + questions[questionsArray[counter]]);
+    session.send(questionsArray[++counter]);
+    if (counter >= questionsArray.length) {
+        session.send('Congratulations. You have completed the interview. Thank you for your time.');
+        counter = 0;
+    }
     // session.beginDialog('StartInterview');
 }).triggerAction({
     matches: 'Bad'
 });
-
-
-// bot.dialog('SearchHotels', [
-//     function (session, args, next) {
-//         session.send('Welcome to the Hotels finder! We are analyzing your message: \'%s\'', session.message.text);
-
-//         // try extracting entities
-//         var cityEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'builtin.geography.city');
-//         var airportEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'AirportCode');
-//         if (cityEntity) {
-//             // city entity detected, continue to next step
-//             session.dialogData.searchType = 'city';
-//             next({ response: cityEntity.entity });
-//         } else if (airportEntity) {
-//             // airport entity detected, continue to next step
-//             session.dialogData.searchType = 'airport';
-//             next({ response: airportEntity.entity });
-//         } else {
-//             // no entities detected, ask user for a destination
-//             builder.Prompts.text(session, 'Please enter your destination');
-//         }
-//     },
-//     function (session, results) {
-//         var destination = results.response;
-
-//         var message = 'Looking for hotels';
-//         if (session.dialogData.searchType === 'airport') {
-//             message += ' near %s airport...';
-//         } else {
-//             message += ' in %s...';
-//         }
-
-//         session.send(message, destination);
-
-//         // Async search
-//         Store
-//             .searchHotels(destination)
-//             .then(function (hotels) {
-//                 // args
-//                 session.send('I found %d hotels:', hotels.length);
-
-//                 var message = new builder.Message()
-//                     .attachmentLayout(builder.AttachmentLayout.carousel)
-//                     .attachments(hotels.map(hotelAsAttachment));
-
-//                 session.send(message);
-
-//                 // End
-//                 session.endDialog();
-//             });
-//     }
-// ]).triggerAction({
-//     matches: 'SearchHotels',
-//     onInterrupted: function (session) {
-//         session.send('Please provide a destination');
-//     }
-// });
-
-// bot.dialog('ShowHotelsReviews', function (session, args) {
-//     // retrieve hotel name from matched entities
-//     var hotelEntity = builder.EntityRecognizer.findEntity(args.intent.entities, 'Hotel');
-//     if (hotelEntity) {
-//         session.send('Looking for reviews of \'%s\'...', hotelEntity.entity);
-//         Store.searchHotelReviews(hotelEntity.entity)
-//             .then(function (reviews) {
-//                 var message = new builder.Message()
-//                     .attachmentLayout(builder.AttachmentLayout.carousel)
-//                     .attachments(reviews.map(reviewAsAttachment));
-//                 session.endDialog(message);
-//             });
-//     }
-// }).triggerAction({
-//     matches: 'ShowHotelsReviews'
-// });
-
-// bot.dialog('Help', function (session) {
-//     session.endDialog('Hi! Try asking me things like \'search hotels in Seattle\', \'search hotels near LAX airport\' or \'show me the reviews of The Bot Resort\'');
-// }).triggerAction({
-//     matches: 'Help'
-// });
-
-
-
-// Spell Check
-if (process.env.IS_SPELL_CORRECTION_ENABLED === 'true') {
-    bot.use({
-        botbuilder: function (session, next) {
-            spellService
-                .getCorrectedText(session.message.text)
-                .then(function (text) {
-                    session.message.text = text;
-                    next();
-                })
-                .catch(function (error) {
-                    console.error(error);
-                    next();
-                });
-        }
-    });
-}
